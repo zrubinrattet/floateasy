2608955b96fb99b56eaaef2c7878a0864a342563
Kyle <kyle.marcy@gmail.com>
2017-06-23 20:48:09 -0400

    didnt get to do any work today, about to crack on testcpt

--- wp-content/themes/floateasy/archive-testimonials_cpt.php
+++ wp-content/themes/floateasy/archive-testimonials_cpt.php
@@ -20,34 +20,22 @@
 
 				<div class="blog-posts-post testimonials_cpt-posts-video">				
 					<?php
-
-					// get iframe HTML
 					$iframe = get_field('youtube_link');
-
-
 					// use preg_match to find iframe src
 					preg_match('/src="(.+?)"/', $iframe, $matches);
 					$src = $matches[1];
-
-
 					// add extra params to iframe src
 					$params = array(
 					    'controls'    => 0,
 					    'hd'        => 1,
 					    'autohide'    => 1
 					);
-
 					$new_src = add_query_arg($params, $src);
-
 					$iframe = str_replace($src, $new_src, $iframe);
-
-
 					// add extra attributes to iframe html
 					$attributes = 'frameborder="0" class="lazyload"'. ' ' . 'data-src="' . $matches[1] . '"';
-
-					$iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
-
-
+					// cache modified iframe var
+					$iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);//
 					// echo $iframe
 					echo $iframe;
 
