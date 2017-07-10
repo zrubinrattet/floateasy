ad0a73f9d9cb2bd68f1714886c6ac6e0eae64858
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-07-09 21:27:13 -0700

    made home hero c2a not open in a new tab

--- wp-content/themes/floateasy/modules/home-hero.php
+++ wp-content/themes/floateasy/modules/home-hero.php
@@ -1,7 +1,7 @@
 
 <section class="hero">
 
-	<a target="_blank" href="<?php echo site_url('/book-an-appointment/') ?>" class="hero-button">Book Your First Float FREE!</a>
+	<a href="<?php echo site_url('/book-an-appointment/') ?>" class="hero-button">Book Your First Float FREE!</a>
 
 
 	<div class="hero-image">
